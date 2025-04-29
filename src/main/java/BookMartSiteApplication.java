acb50fc274eede3dc3333268676d2b5b65d89ab9
chamidukeshika <chamidukeshikaz@gmail.com>
2025-04-29 12:10:15 +0530

    cart model create commit
    cart model and folder structure create for packges

---
+++ src/main/java/BookMartSiteApplication.java
@@ -0,0 +1,11 @@
+package com.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMartSiteApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BookMartSiteApplication.class, args);
+	}
+}
