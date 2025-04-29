91abcac069b6d31ba257353d2ab534a6e3c401ba
chamidukeshika <chamidukeshikaz@gmail.com>
2025-04-29 19:15:26 +0530

    update home controller
    product mappings

--- java/com/ecom/controller/HomeController.java
+++ java/com/ecom/controller/HomeController.java
@@ -1,5 +1,160 @@
 package com.ecom.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.security.Principal;
+import java.util.Comparator;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.ecom.model.Category;
+import com.ecom.model.Product;
+import com.ecom.model.ReadOnline;
+import com.ecom.model.UserDtls;
+import com.ecom.service.CartService;
+import com.ecom.service.CategoryService;
+import com.ecom.service.ProductService;
+import com.ecom.service.ReadOnlineService;
+import com.ecom.service.UserService;
+import com.ecom.util.CommonUtil;
+
+import io.micrometer.common.util.StringUtils;
+import jakarta.mail.MessagingException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/")
 public class HomeController {
-    
+
+	@Autowired
+	private CategoryService categoryService;
+
+	@Autowired
+	private ProductService productService;
+
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private CartService cartService;
+
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+
+	@Autowired
+	private CommonUtil commonUtil;
+
+	@ModelAttribute
+	public void getUserDetails(Principal p, Model m) {
+
+		if (p != null) {
+
+			String email = p.getName();
+			UserDtls userDtls = userService.getUserByEmail(email);
+			m.addAttribute("user", userDtls);
+			Integer countCart = cartService.getCountCart(userDtls.getId());
+			m.addAttribute("countCart", countCart);
+		}
+		List<Category> allActiveCategory = categoryService.getAllActiveCategory();
+		m.addAttribute("categorys", allActiveCategory);
+
+	}
+
+	@GetMapping("/")
+	public String index(Model m) {
+
+		List<Category> allActiveCategory = categoryService.getAllActiveCategory().stream()
+				.sorted(Comparator.comparing(Category::getId)) // Sorting categories by id
+				.limit(6).toList();
+
+		List<Product> allActiveProducts = productService.getAllActiveProducts("").stream()
+				.sorted(Comparator.comparing(Product::getId)) // Sorting products by id
+				.limit(8).toList();
+
+		m.addAttribute("category", allActiveCategory);
+		m.addAttribute("products", allActiveProducts);
+
+		return "index";
+	}
+
+	@GetMapping("/signin")
+
+	public String login() {
+		return "login";
+	}
+
+	@GetMapping("/register")
+	public String register() {
+		return "register";
+	}
+
+	@GetMapping("/products")
+	public String products(Model m, @RequestParam(value = "category", defaultValue = "") String category,
+			@RequestParam(name = "pageNo", defaultValue = "0") Integer pageNo,
+			@RequestParam(name = "pageSize", defaultValue = "4") Integer pageSize,
+			@RequestParam(defaultValue = "") String ch) {
+
+		List<Category> categories = categoryService.getAllActiveCategory();
+		m.addAttribute("paramValue", category);
+		m.addAttribute("categories", categories);
+
+//		List<Product> products = productService.getAllActiveProducts(category);
+//		m.addAttribute("products", products);
+		Page<Product> page = null;
+		if (StringUtils.isEmpty(ch)) {
+			page = productService.getAllActiveProductPagination(pageNo, pageSize, category);
+		} else {
+			page = productService.searchActiveProductPagination(pageNo, pageSize, category, ch);
+		}
+
+		List<Product> products = page.getContent();
+		m.addAttribute("products", products);
+		m.addAttribute("productsSize", products.size());
+
+		m.addAttribute("pageNo", page.getNumber());
+		m.addAttribute("pageSize", pageSize);
+		m.addAttribute("totalElements", page.getTotalElements());
+		m.addAttribute("totalPages", page.getTotalPages());
+		m.addAttribute("isFirst", page.isFirst());
+		m.addAttribute("isLast", page.isLast());
+
+		return "product";
+	}
+
+	@GetMapping("/product/{id}")
+	public String product(@PathVariable int id, Model m) {
+
+		Product productById = productService.getProductById(id);
+		m.addAttribute("product", productById);
+		return "view_product";
+	}
+  
 }
