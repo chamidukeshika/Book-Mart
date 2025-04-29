5c087d6682f11f534dbdf360b2243881a93b8efd
Lakshitha Udaranga <165992193+LakshithaUdar@users.noreply.github.com>
2025-04-29 19:06:47 +0530

    Read Online Service Creation

---
+++ java/com/ecom/service/ReadOnlineService.java
@@ -0,0 +1,24 @@
+// ReadOnlineService.java
+package com.ecom.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import com.ecom.model.ReadOnline;
+
+public interface ReadOnlineService {
+	ReadOnline saveDocument(ReadOnline doc);
+
+	Page<ReadOnline> getAllDocuments(String category, int pageNo, int pageSize);
+
+	Page<ReadOnline> searchDocuments(String query, int pageNo, int pageSize);
+
+	void deleteDocument(int id);
+
+	ReadOnline getDocumentById(int id);
+
+	List<ReadOnline> getAllDocuments();
+
+	ReadOnline updateDocument(ReadOnline document);
+}
