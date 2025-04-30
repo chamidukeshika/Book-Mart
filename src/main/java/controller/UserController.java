65ce3d98c937e8c9c0b321865ede7a4748d4add3
chamidukeshika <chamidukeshikaz@gmail.com>
2025-04-30 14:00:46 +0530

    user controller bug fix

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+
 
 import com.ecom.model.Cart;
 import com.ecom.model.Category;
